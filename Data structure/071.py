42894c4885dc7a9606c1c0cb7cbefdc77dbe5e4a
Nistizia <khanitthachun33@gmail.com>
2021-07-27 14:39:22 +0700

    27/07/2021

---
+++ Data structure/071.py
@@ -0,0 +1,5 @@
+# ข้อ 71
+country = ("America","Brazil","China","Dominican","Egypt")
+print(country)
+print(type(country))
+# --------------------
---
+++ Data structure/075.py
@@ -0,0 +1,4 @@
+# ข้อ 75
+country = ("America","Brazil","China","Dominican","Egypt")
+print(country[-1])
+# --------------------
